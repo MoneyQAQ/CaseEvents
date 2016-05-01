f1ca55f30cf793136310a710aaeb05ead8c3064f
Dyanngg <yxd155@case.edu>
2016-05-01 13:14:03 -0400

    secondviewcontroller updates

--- CaseEvents/SecondViewController.swift
+++ CaseEvents/SecondViewController.swift
@@ -8,16 +8,28 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    let ref = Firebase(url: "https://flickering-heat-8881.firebaseio.com")
+    var eventCount:UInt = 0
+    var eref = Firebase(url: "https://flickering-heat-8881.firebaseio.com/Events")
+    var events = [NSDictionary]()
+    
+    let ref = Firebase(url: "https://flickering-heat-8881.firebaseio.com/Users")
     
     var userData = UserModel.currentUser.providerData
 
     override func viewDidLoad() {
         super.viewDidLoad()
         let email = userData["email"] as? String
+        let uid = UserModel.currentUser.uid
         test.text = email
+        
+        let userimageurl = userData["profileImageURL"] as? String
+        if let checkedUrl = NSURL(string: userimageurl!) {
+            profileimage.contentMode = .ScaleAspectFit
+            downloadImage(checkedUrl)
+        }
+        
         let newUserInfo = ["email": email!, "age": "19"]
         let newRef = ref.childByAutoId()
         newRef.setValue(newUserInfo)
@@ -29,9 +41,69 @@ class SecondViewController: UIViewController {
                 }
             }
         })
+        
+        self.favs.registerClass(EventCell.self, forCellReuseIdentifier: "myeventcell")
+        eref.observeEventType(.Value, withBlock: { snapshot in
+            self.eventCount = snapshot.childrenCount
+        })
+        loadDataFromFirebase()
+        
         print(userData)
     }
 
+    func getDataFromUrl(url:NSURL, completion: ((data: NSData?, response: NSURLResponse?, error: NSError? ) -> Void)) {
+        NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) in
+            completion(data: data, response: response, error: error)
+            }.resume()
+    }
+    
+    func downloadImage(url: NSURL){
+        print("Download Started")
+        print("lastPathComponent: " + (url.lastPathComponent ?? ""))
+        getDataFromUrl(url) { (data, response, error)  in
+            dispatch_async(dispatch_get_main_queue()) { () -> Void in
+                guard let data = data where error == nil else { return }
+                print(response?.suggestedFilename ?? "")
+                print("Download Finished")
+                self.profileimage.image = UIImage(data: data)
+            }
+        }
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let event = events[indexPath.row]
+        let dequeued: AnyObject = tableView.dequeueReusableCellWithIdentifier("myeventcell", forIndexPath: indexPath)
+        let cell = dequeued as! EventCell
+        cell.eventName.text = event["name"] as? String
+        cell.location.text = event["location"] as? String
+        return cell
+    }
+    
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return events.count
+    }
+    
+    
+    func loadDataFromFirebase() {
+        eref.observeEventType(.Value, withBlock: { snapshot in
+            var tempItems = [NSDictionary]()
+            for item in snapshot.children {
+                let child = item as! FDataSnapshot
+                let name = child.value as! NSDictionary
+                tempItems.append(name)
+            }
+            self.events = tempItems
+            self.favs.reloadData()
+        })
+    }
+
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -39,6 +111,8 @@ class SecondViewController: UIViewController {
 
     @IBOutlet weak var test: UILabel!
     @IBOutlet weak var test2: UILabel!
+    @IBOutlet weak var profileimage: UIImageView!
+    @IBOutlet weak var favs: UITableView!
     
 
 }
