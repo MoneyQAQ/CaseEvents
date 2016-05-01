d454329883bd89ac1ccd8ccb4540df93336d3fd3
Dyanngg <yxd155@case.edu>
2016-05-01 02:46:19 -0400

    More firebase and user profile view

--- CaseEvents/SecondViewController.swift
+++ CaseEvents/SecondViewController.swift
@@ -9,10 +9,27 @@
 import UIKit
 
 class SecondViewController: UIViewController {
+    
+    let ref = Firebase(url: "https://flickering-heat-8881.firebaseio.com")
+    
+    var userData = UserModel.currentUser.providerData
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let email = userData["email"] as? String
+        test.text = email
+        let newUserInfo = ["email": email!, "age": "19"]
+        let newRef = ref.childByAutoId()
+        newRef.setValue(newUserInfo)
+        ref.observeEventType(.Value, withBlock: { snapshot in
+            for item in snapshot.children {
+                let child = item as! FDataSnapshot
+                if child.value.valueForKey("email") as? String == email {
+                    self.test2.text = child.value.valueForKey("age") as? String
+                }
+            }
+        })
+        print(userData)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +37,9 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var test: UILabel!
+    @IBOutlet weak var test2: UILabel!
+    
 
 }
 
---
+++ CaseEvents/UserModel.swift
@@ -0,0 +1,15 @@
+//
+//  UserModel.swift
+//  CaseEvents
+//
+//  Created by A❤Y on 5/1/16.
+//  Copyright © 2016 Xiaohan Wang. All rights reserved.
+//
+
+import Foundation
+
+class UserModel {
+    
+    static var currentUser = FAuthData()
+
+}
