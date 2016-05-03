3783a89c6c01256a9ca19c4d19f5670c45695648
Dyanngg <yxd155@case.edu>
2016-05-02 20:50:59 -0400

    outlets

--- CaseEvents/EventDetailVC.swift
+++ CaseEvents/EventDetailVC.swift
@@ -1,5 +1,5 @@
 //
-//  FavEventCell.swift
+//  EventDetailVC.swift
 //  CaseEvents
 //
 //  Created by A❤Y on 5/2/16.
@@ -8,17 +8,32 @@
 
 import UIKit
 
-class FavEventCell: UITableViewCell {
-
+class EventDetailVC: UIViewController {
+    
     @IBOutlet weak var eventName: UILabel!
     @IBOutlet weak var oName: UILabel!
     @IBOutlet weak var location: UILabel!
     @IBOutlet weak var date: UILabel!
     @IBOutlet weak var month: UILabel!
+    @IBOutlet weak var des: UITextView!
+    
+    @IBOutlet weak var cost: UILabel!
+    
+    @IBAction func shareOnFacebook(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func shareOnTwitter(sender: AnyObject) {
+    }
     
     @IBAction func add2Calendar(sender: AnyObject) {
     }
-    @IBAction func addToFav(sender: AnyObject) {
+    
+    @IBAction func addToFavs(sender: AnyObject) {
     }
+    
+    
+    
+    
     
 }
